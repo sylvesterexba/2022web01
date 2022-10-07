8f58c6d71e887392ed0c65f85dbc462fa50f02fc
Sylvester Chen <sylvesterexba@gmail.com>
2022-10-07 15:12:38 +0800

    初始化DB 類別

---
+++ base.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class DB{
+  private $dsn="mysql:host=localhost;charset=utf8;dbname=db_story";
+  private $root='root';
+  private $password='12345';
+  private $table;
+  private $pdo;
+
+  public function __construct($table){
+    $this->table=$table;
+    $this->pdo=new PDO($this->dsn,$this->root,$this->password);
+  }
+
+  public function all(...$arg){
+    $sql="select * from $this->table ";
+
+
+
+    return $this->pdo->query($sql)->fetchAll();
+  }
+}
+
+$db=new DB("user");
+echo "<pre>";
+print_r($db->all());
+echo "</pre>";
+
+$db2=new DB("stories");
+echo "<pre>";
+print_r($db2->all());
+echo "</pre>";
+?>
